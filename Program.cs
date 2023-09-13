46e73b7ef9c2dd2610bb75b0cf060796182cfc6e
339531824 <339531824@2260I-L29-011>
2023-09-13 10:07:18 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intro_To_Methods_Programming_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DrawSW();
+            Console.ReadLine();
+        }
+        public static void DrawSW()
+        {
+            Console.Write("               ________");
+            Console.WriteLine("\t\t\t\t       ________________");
+            Console.Write("          _,.-Y  |  |  Y-._");
+            Console.WriteLine("\t\t\t     |'-.--._ _________:");
+            Console.Write("      .-~' ||  |  |  |   '-.");
+            Console.WriteLine("\t\t\t     |  /    |  __    __/");
+            Console.Write("      I' ''=='|' !''! '|'[]''|     _____");
+            Console.WriteLine("\t     | |  _  | [/_/= [/_/");
+            Console.Write("      L__  [] |..------|:   _[----I'' .-{''-.");
+            Console.WriteLine("\t     | |.' '. /.........|");
+            Console.Write("     I___ |  ..| l______|l_ [__L]_[I_/r(=}=-P");
+            Console.WriteLine("\t     | ( <)  ||:       :|");
+            Console.Write("    [L______L_[________]______j~  '-=c_]/=-^");
+            Console.WriteLine("\t      / '._.' | :.....: |_(o");
+            Console.Write("     /_I_j.--./==I|I==_/.--L_]");
+            Console.WriteLine("\t\t\t       '-//_   // .------./");
+            Console.Write("       [_((==)[`-----'](==)j");
+            Console.WriteLine("\t\t\t       _   /   ||.---.||  _");
+            Console.Write("          I--I'~~'''~~'I--I");
+            Console.WriteLine("\t\t\t      / /  '-._|//n~~/n' | /");
+            Console.Write("          |[]|         |[]|");
+            Console.WriteLine("\t\t\t     (| []=.--[===[()]===[) |");
+            Console.Write("          l__j         l__j");
+            Console.WriteLine("\t\t\t     </_/  /_______/ _.' /_/");
+            Console.Write("          | !!|         |!!|");
+            Console.WriteLine("\t\t\t     ///            (_/_/");
+            Console.Write("          | ..|         |..|");
+            Console.WriteLine("\t\t\t      |/            [/");
+            Console.Write("          ([])         ([])");
+            Console.WriteLine("\t\t\t     ||:|           | I|");
+            Console.Write("          ]--[         ]--[");
+            Console.WriteLine("\t\t\t     |::|           | I|");
+            Console.Write("          [_L]         [_L]");
+            Console.WriteLine("\t\t\t     ||:|           | I|");
+            Console.Write("         /|..|/       /|..|/");
+            Console.WriteLine("\t\t\t     ||:|           : /:");
+            Console.Write("        `=}--{='     `=}--{='");
+            Console.WriteLine("\t\t\t     |/:|            /I|");
+            Console.Write("       .-^--r-^-.   .-^--r-^-.");
+            Console.WriteLine("\t\t\t     ://:            ([])");
+            Console.WriteLine("\t\t\t\t\t\t     ([])             [|");
+            Console.WriteLine("\t\t\t\t\t\t      ||              |/_");
+            Console.WriteLine("\t\t\t\t\t\t     _/_/_            [ -'-.__");
+            Console.WriteLine("\t\t\t\t\t\t    <]   />            /_____.>");
+            Console.WriteLine("\t\t\t\t\t\t      /__/");
+        }
+        public static void DrawComputer()
+        {
+            Console.WriteLine("");
+        }
+    }
+}
